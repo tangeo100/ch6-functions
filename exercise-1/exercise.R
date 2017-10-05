cf964a6d7957e8c8dd74fc231a7840f68b5bc015
tangeo100 <rnld.tang@gmail.com>
2017-10-05 14:27:09 -0700

    change exercise.R in exercise-1 and change exercise.R in exercise-2

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,14 +2,21 @@
 
 # Write a function `AddThree` that adds 3 to an input value
 
+AddThree <- function(input){
+  return (input + 3)
+  
+}
 
 # Create a variable `ten` by passing 7 to your `AddThree` function
-
+ten <- AddThree(7)
 
 # Write a function `FeetToMeters` that converts from feet to meters
-
+FeetToMeters <- function(feet){
+  return (0.3048 * feet)
+}
 
 # Create a variable `height.in.feet` that is your height in feet
-
+height.in.feet <- 5.66667
 
 # Create a variable `height.in.meters` by passing `height.in.feet` to your `FeetToMeters` function
+height.in.meters <- FeetToMeters(height.in.feet)
--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -2,19 +2,47 @@
 
 # Write a function `CompareLength` that takes in 2 vectors, and returns the sentence:
 # "The difference in lengths is N"
-
+CompareLength <- function(vector1, vector2){
+  difference <- 0
+  if(length(vector1) > length(vector2)){
+    difference <- length(vector1) - length(vector2)
+  } else {
+    difference <- length(vector2) - length(vector1)
+  }
+  paste('The difference in lengths is', difference)
+}
 
 # Pass two vectors of different length to your `CompareLength` function
-
+vector1 <- c(1, 2, 3)
+vector2 <- c(1, 2)
+CompareLength(vector1, vector2)
 
 # Write a function `DescribeDifference` that will return one of the following statements:
 # "Your first vector is longer by N elements"
 # "Your second vector is longer by N elements"
-
+DescribeDifference <- function(vector1, vector2){
+  if(length(vector1) >= length(vector2)){
+    difference <- length(vector1) - length(vector2)
+    paste("Your first vector is longer by ", difference, " elements")
+  } else {
+    difference <- length(vector2) - length(vector1)
+    paste("Your second vector is longer by ", difference, " elements")
+  }
+}
 
 # Pass two vectors to your `DescribeDifference` function
-
+DescribeDifference(vector1, vector2)
 
 ### Bonus ###
 
 # Rewrite your `DescribeDifference` function to tell you the name of the vector which is longer
+
+DescribeDifference <- function(vector1, vector2){
+  if(length(vector1) >= length(vector2)){
+    difference <- length(vector1) - length(vector2)
+    paste("vector1", " is longer by ", difference, " elements")
+  } else {
+    difference <- length(vector2) - length(vector1)
+    paste("vector2", " is longer by ", difference, " elements")
+  }
+}
